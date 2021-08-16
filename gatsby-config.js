383dbcbb937e090f606f24657da9ed2941d439d8
hikaori <kaori55194@gmail.com>
2021-08-16 21:35:48 +0900

    add StaticImage

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,19 +1,21 @@
 module.exports = {
   siteMetadata: {
-    siteUrl: "https://www.yourdomain.tld",
-    title: "My Gatsby Site",
+    siteUrl: 'https://www.yourdomain.tld',
+    title: 'My Gatsby Site',
   },
   plugins: [
-    "gatsby-plugin-gatsby-cloud",
-    "gatsby-plugin-react-helmet",
-    "gatsby-plugin-mdx",
+    'gatsby-plugin-gatsby-cloud',
+    'gatsby-plugin-image',
+    'gatsby-plugin-sharp',
+    'gatsby-plugin-react-helmet',
+    'gatsby-plugin-mdx',
     {
-      resolve: "gatsby-source-filesystem",
+      resolve: 'gatsby-source-filesystem',
       options: {
-        name: "pages",
-        path: "./src/pages/",
+        name: 'pages',
+        path: './src/pages/',
       },
-      __key: "pages",
+      __key: 'pages',
     },
   ],
 };
